06fb367de726074b19c79c9d33e00b5da474a0ac
JuanPaz14 <camilopaz389@gmail.com>
2024-10-12 02:01:15 -0500

    terminado el menu lateral

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -1,9 +1,15 @@
+'use client'
+
 import { TitleFont } from '@/config/fonst'
 import Link from 'next/link'
 import React from 'react'
 import { IoCartOutline, IoSearchOutline } from 'react-icons/io5'
+import { useUIStore } from "@/store";
 
 export const TopMenu = () => {
+
+    const openSideMenu = useUIStore(state => state.openSideMenu);
+
   return (
     <nav className='flex px-5 justify-between items-center w-full'>
         {/*logo*/}
@@ -33,7 +39,7 @@ export const TopMenu = () => {
                     <IoCartOutline className='w-5 h-5'/>
                 </div>    
             </Link>
-            <button className='m-2 p-2 rounded-md transition-all hover:bg-gray-100'>
+            <button className='m-2 p-2 rounded-md transition-all hover:bg-gray-100' onClick={()=>openSideMenu()}>
                 Menú
             </button>
         </div>
