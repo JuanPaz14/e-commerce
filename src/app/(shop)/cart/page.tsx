d443bb773e0f5fa9d7151de8e538f51e5e62be5b
JuanPaz14 <camilopaz389@gmail.com>
2024-10-12 21:40:46 -0500

    terminando carrito

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -10,6 +10,7 @@ export default function () {
     initialData.products[0],
     initialData.products[1],
     initialData.products[2],
+  
   ]
 
 
@@ -28,30 +29,60 @@ export default function () {
               <Link href='/' className='underline mb-5'>
                 Continua comprando
               </Link>
-
-            </div>
-
-            {/*Items*/}
-            {
-              productsInCart.map(product => (
-                <div key={product.slug} className='flex'>
-                  <Image
-                    src={`/products/${product.images[0]}`}
-                    width={100}
-                    height={100}
-                    alt={product.title}
-                    className='mr-5 rounded'
-                  />
-                  <div>
-                    <p>{product.title}</p>
-                    <p>{product.price}</p>
-                    <QuantitySelector quantity={3}/>
-                    <button className='underline mt-3'>remover</button>
+ 
+              {/*Items*/}
+              {
+                productsInCart.map(product => (
+                  <div key={product.slug} className='flex mb-5'>
+                    <Image
+                      src={`/products/${product.images[0]}`}
+                      width={100}
+                      height={100}
+                      style={{
+                        width:'100px',
+                        height:'100px'
+                      }}
+                      alt={product.title}
+                      className='mr-5 rounded'
+                    />
+                    <div>
+                      <p>{product.title}</p>
+                      <p>{product.price}</p>
+                      <QuantitySelector quantity={3}/>
+                      <button className='underline mt-3'>remover</button>
+                    </div>
                   </div>
+                ))
+              }
+              </div> 
+        
+              {/*CheckOut - resumen de orden*/}
+              <div className='bg-white rounded-xl shadow-xl p-7'>
+                <h2 className='text-2xl mb-2 '>Resumen de Ordén</h2>
+                <div className='grid grid-cols-2'>
+                  <span>No. Productos</span>
+                  <span className='text-right'>3 articulos</span>
+
+                  <span>subTotal</span>
+                  <span className='text-right'>$100</span>
+
+                  <span>Inpuestos (15%)</span>
+                  <span className='text-right'>$100</span>
+
+                  <span className='mt-5 text-2xl '>Total</span>
+                  <span className=' mt-5 text-2xl text-right'>$100</span>
                 </div>
-              ))
-            }
-            {/*CheckOut */}
+                <div className='mt-5 mb-2 wfull'>
+                  <Link 
+                    className='flex btn-primary justify-center'
+                    href="/checkout/address">
+                    Checkout
+                  </Link>
+                </div>
+
+              </div>
+
+
         </div>
       </div>
     </div>
